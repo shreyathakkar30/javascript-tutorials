ef0addae40f7359b753591f3c27a406a78c7b23a
Shreya <shreyathakkar2004@gmail.com>
2026-05-28 12:29:14 +0530

    async IIFE in JavaScript

--- js/iife/script.js
+++ js/iife/script.js
@@ -1,7 +1,12 @@
 //IIFE - Immediately Invoked Function Expression
 (function add (a, b){
+    let age = 30;
     console.log(a + b);
+    console.log(age);
 })(6, 2);
+
+//async IIFE
+const data = (async () => await fetch("https://domain.name/path/file.ext")) ();
 
 (function saySomething(){
     console.log("Hello World");
